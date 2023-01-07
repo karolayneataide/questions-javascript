c473f59e69a6695dcf26c2eedff926d80bd6f80a
Karolayne Ataide <karolayne.o.v.a@gmail.com>
2023-01-07 00:11:09 -0300

    add task 49-50

--- Lista 1 - Variáveis e Expressões/questão51.js
+++ Lista 1 - Variáveis e Expressões/questão51.js
@@ -3,8 +3,3 @@ const prompt = require("prompt-sync")();
 console.log(
   "Escreva um programa que leia as coordenadas x e y de pontos no R2 e calcule sua distancia da origem (0, 0)."
 );
-
-const x = Number(prompt("Digite o valor de X: "));
-const y = Number(prompt("Digite o valor de Y: "));
-
-const distancia = console.log(anoNascimento);
