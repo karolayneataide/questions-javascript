4e477e424ec544f6eaf3d33c5f64d3b618f3cc45
Karolayne Ataide <karolayne.o.v.a@gmail.com>
2023-01-19 16:33:46 -0300

    add tasks 25-38

---
+++ Lista 2 - Comandos Condicionais/questão38.js
@@ -0,0 +1,85 @@
+const prompt = require("prompt-sync")();
+
+const data = prompt("digite sua data de nascimento no estilo dd/mm/aaaa: ");
+
+const dia = data.slice(0, 2);
+const mes = data.slice(3, 5);
+const ano = data.slice(6, 10);
+
+const diaEValido = dia > 0 && dia <= 31;
+const mesEValido = mes > 0 && mes <= 12;
+const anoEValido = ano <= 2023 && ano.length === 4;
+const eBissexto = (ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0;
+
+const dia30 = dia > 0 && dia < 31;
+const mes31 =
+  mes == 01 ||
+  mes == 03 ||
+  mes == 05 ||
+  mes == 07 ||
+  mes == 08 ||
+  mes == 10 ||
+  mes == 12;
+const mes30 = mes == 04 || mes == 06 || mes == 09 || mes == 11;
+
+const mesFevereiro = mes == 02;
+const diaFevereiroBissexto = dia > 0 && dia <= 29;
+const diaFevereiro = dia > 0 && dia <= 28;
+
+if (!diaEValido || !mesEValido || !anoEValido) {
+  return console.log("Data inválida");
+} else {
+  if (eBissexto) {
+    if (mesEValido) {
+      if (mes31) {
+        if (diaEValido) {
+          return console.log("Data válida");
+        } else {
+          return console.log("Data inválida");
+        }
+      }
+      if (mes30) {
+        if (dia30) {
+          return console.log("Data válida");
+        } else {
+          return console.log("Data inválida");
+        }
+      }
+      if (mesFevereiro) {
+        if (diaFevereiroBissexto) {
+          return console.log("Data válida");
+        } else {
+          return console.log("Data inválida");
+        }
+      }
+    } else {
+      return console.log("data inválida");
+    }
+  } else {
+    if (mesEValido) {
+      if (mes31) {
+        if (diaEValido) {
+          return console.log("Data válida");
+        } else {
+          return console.log("Data inválida");
+        }
+      }
+      if (mes30) {
+        if (dia30) {
+          return console.log("Data válida");
+        } else {
+          return console.log("Data inválida");
+        }
+      }
+      if (mesFevereiro) {
+        if (diaFevereiro) {
+          return console.log("Data válida");
+        } else {
+          return console.log("Data inválida");
+        }
+      }
+    } else {
+      return console.log("data inválida");
+    }
+  }
+}
